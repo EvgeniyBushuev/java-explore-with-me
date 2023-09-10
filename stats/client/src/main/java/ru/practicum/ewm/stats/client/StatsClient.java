aea9c26127e1864820946dbe0f0de42023d640b7
EvgeniyBushuev <johnyk359@yandex.ru>
2023-09-10 16:08:33 +0300

    тест

--- stats/client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
+++ stats/client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
@@ -5,9 +5,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import ru.practicum.ewm.stats.client.BaseClient;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +18,7 @@ public class StatsClient extends BaseClient {
 
     private static final String API_PREFIX = "/hit";
     private static final String GET_STAT_PATH = "/stats?start={start}&end={end}&uris={uris}&unique={unique}";
+    private static final DateFormat DF = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
@@ -29,11 +33,18 @@ public class StatsClient extends BaseClient {
         return post(API_PREFIX, hitDto);
     }
 
-    public ResponseEntity<Object> getStats(String start, String end, List<String> uris, boolean unique) {
+    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+
+        String urisStr = String.join(",", uris);
+
+        String startStr = DF.format(start);
+
+        String endStr = DF.format(end);
+
         Map<String, Object> parameters = Map.of(
-                "start", start,
-                "end", end,
-                "uris", uris,
+                "start", startStr,
+                "end", endStr,
+                "uris", urisStr,
                 "unique", unique
         );
         return get(GET_STAT_PATH, parameters);
