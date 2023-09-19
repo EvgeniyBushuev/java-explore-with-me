20e93e96cd4eed425e6b02ab600b5edf172406f7
EvgeniyBushuev <johnyk359@yandex.ru>
2023-09-19 20:31:39 +0300

    test

--- service/src/main/java/ru/practicum/ewm/service/controller/user/UserControllerAdmin.java
+++ service/src/main/java/ru/practicum/ewm/service/controller/user/UserControllerAdmin.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.service.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +10,6 @@ import ru.practicum.ewm.service.service.UserService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @RestController
