c11491937028d802a944559f9bf49955ccaed1c1
EvgeniyBushuev <johnyk359@yandex.ru>
2023-09-20 20:15:32 +0300

    test

--- service/src/main/java/ru/practicum/ewm/service/dto/event/UserUpdateEventDto.java
+++ service/src/main/java/ru/practicum/ewm/service/dto/event/UserUpdateEventDto.java
@@ -20,18 +20,27 @@ import java.time.LocalDateTime;
 public class UserUpdateEventDto {
     @Size(min = 3, max = 120)
     private String title;
+
     @Size(min = 20, max = 2000)
     private String annotation;
+
     @Size(min = 20, max = 7000)
     private String description;
+
     private Long category;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
+
     private Location location;
-    private Boolean paid;
+
+    private Boolean isPaid;
+
     @PositiveOrZero
     private Integer participantLimit;
+
     private Boolean requestModeration;
+
     private String stateAction;
 }
